0073bbb7671800364fefc8e4c5a5d5dee1137bbc
Shido <shido.spb@gmail.com>
2017-11-25 17:31:57 +0200

    Fixed poller for 0 results

--- ebay_poller.py
+++ ebay_poller.py
@@ -9,10 +9,14 @@ def get_price(name):
     x=data.content
     tree  = html.fromstring(x)
     try:
-        price = tree.find_class('prc')[0].find_class('bold')[0].text
+        count = int(tree.find_class('rcnt')[0].text)
+        if count > 0:
+            price = tree.find_class('prc')[0].find_class('bold')[0].text.split()[0]
+        else:
+            price = None
     except IndexError as e:
         return None
-    return price.split()[0]
+    return price
 
 def get_url(item):
     enc_item = urlencode({'_nkw': item})
